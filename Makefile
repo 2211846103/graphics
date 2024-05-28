9c661033063d84f81295b5f32ec363e9c40b60b0
MIbrahim9276 <m.ibrahm9276@gmail.com>
2024-05-28 22:16:03 +0200

    Currently WIP

--- Makefile
+++ Makefile
@@ -2,21 +2,19 @@ PROJECT := graphics
 
 BUILD_DIR := build
 
-INCLUDE_DIR := ./include
-SRC_DIR := ./src
-LIB_DIR := ./lib
+INCLUDE_DIR := include
+SRC_DIR := src
+LIB_DIR := libs
 
-LIBS_ARGS := 
-
-INCLUDE := $(INCLUDE_DIR)/*
-SRC := $(SRC_DIR)/*
-LIBS := $(LIB_DIR)/*
+INCLUDE := $(wildcard $(INCLUDE_DIR)/*.hpp)
+SRC := $(wildcard $(SRC_DIR)/*.cpp)
+LIBS :=  $(basename $(wildcard *.dll))
 
 install: clean compile
 
 clean:
-	rmdir $(BUILD_DIR)
+	rm -rf $(BUILD_DIR)
 	mkdir $(BUILD_DIR)
 
 compile: $(SRC)
-	g++ $(SRC) -o $(BUILD_DIR)/$(PROJECT)
+	g++ $(SRC) -I ./$(INCLUDE_DIR) -L ./$(LIB_DIR) -lglfw3 -o $(BUILD_DIR)/$(PROJECT)
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,11 +1,35 @@
 // Includes
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 // Main code
 int main(int argc, char* argv[]) {
 
-    // Test
-    std::cout << "Hello World" << std::endl;
+    glfwInit(); // Initialize GLFW.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // For Mac OS add this line.
 
+
+    // Creates a Window.
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Tring to learn OpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Checks and call events and swap the buffers
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    // Cleans/Deletes all GLFW resources that we allocated.
+    glfwTerminate();
     return 0;
 }
