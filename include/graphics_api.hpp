0409a2de19d03b18957a599fe7e4006facfc8409
MIbrahim9276 <m.ibrahm9276@gmail.com>
2024-05-30 20:14:52 +0200

    Implementing graphics api

--- include/graphics_api.hpp
+++ include/graphics_api.hpp
@@ -8,11 +8,14 @@ namespace Graphics {
     struct VertexArray {};
 
     class GraphicsApi {
-        virtual void init();
-        virtual Texture createTexture(char* path);
-        virtual Shader createShader(char* vertexSource, char* fragmentSource);
-        virtual Buffer createBuffer(void* data);
-        virtual VertexArray createVertexArray();
-        virtual void Draw(VertexArray vertexArray, Shader shader, Texture texture);
+        public:
+            virtual void init();
+            virtual Texture createTexture(char* path);
+            virtual Shader createShader(char* vertexSource, char* fragmentSource);
+            virtual Buffer createBuffer(void* data);
+            virtual VertexArray createVertexArray();
+            virtual void Draw(VertexArray vertexArray, Shader shader, Texture texture);
     };
+
+    class OpenGLGraphics: public GraphicsApi {};
 }
