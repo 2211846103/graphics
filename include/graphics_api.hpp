283b8a9485f83919e02de2ba8c4785dd40843c76
MIbrahim9276 <m.ibrahm9276@gmail.com>
2024-05-30 20:06:44 +0200

    Continue Setup

--- include/graphics_api.hpp
+++ include/graphics_api.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
 namespace Graphics {
+    // Boilerplate (Don't mind it)
+    struct Texture {};
+    struct Shader {};
+    struct Buffer {};
+    struct VertexArray {};
+
     class GraphicsApi {
         virtual void init();
+        virtual Texture createTexture(char* path);
+        virtual Shader createShader(char* vertexSource, char* fragmentSource);
+        virtual Buffer createBuffer(void* data);
+        virtual VertexArray createVertexArray();
+        virtual void Draw(VertexArray vertexArray, Shader shader, Texture texture);
     };
 }
