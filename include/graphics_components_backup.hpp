7ebc1ba62a5fce9257b1af9c82e34b6eafb75a16
IbrahimElsharif <Elsharif4458@outlook.com>
2024-06-03 21:04:20 +0200

    WIP

--- include/graphics_components_backup.hpp
+++ include/graphics_components_backup.hpp
@@ -15,6 +15,8 @@ namespace Graphics {
     };
 
     class Buffer {
+        void* data;
+
         virtual void bind(BufferType type) = 0;
         virtual void setData(void* data) = 0;
         virtual void unbind() = 0;
@@ -55,5 +57,5 @@ namespace Graphics {
             void bind() override;
             void unbind() override;
             void draw() override;
-    }
+    };
 }
--- src/graphics_components_backup.cpp
+++ src/graphics_components_backup.cpp
@@ -31,36 +31,36 @@ OpenGLBuffer::OpenGLBuffer(BufferType type, void* data, BufferUsage usage) {
     this->setData(data);
 }
 
-void OpenGLBuffer::bind() {
-    glBindBuffer(this->_type, id);
+void OpenGLBuffer::bind(BufferType type) {
+    glBindBuffer(type, id);
 }
 
 void OpenGLBuffer::bind(BufferType type) {
     switch(type) {
         case VertexBuffer:
-            _type = GL_ARRAY_BUFFER; break;
+            type = GL_ARRAY_BUFFER; break;
         case IndexBuffer:
-            _type = GL_ELEMENT_ARRAY_BUFFER; break;
+            type = GL_ELEMENT_ARRAY_BUFFER; break;
         case UniformBuffer:
-            _type = GL_UNIFORM_BUFFER; break;
+            type = GL_UNIFORM_BUFFER; break;
         case TextureBuffer:
-            _type = GL_TEXTURE_BUFFER; break;
+            type = GL_TEXTURE_BUFFER; break;
         case ShaderStorageBuffer:
-            _type = GL_SHADER_STORAGE_BUFFER; break;
+            type = GL_SHADER_STORAGE_BUFFER; break;
         default:
-            _type = 0;
+            type = 0;
     }
 
-    glBindBuffer(this->_type, this->id);
+    glBindBuffer(type, this->id);
 }
 
 void OpenGLBuffer::unbind() {
-    glBindBuffer(this->_type, 0);
+    glBindBuffer(this->type, 0);
 }
 
 void OpenGLBuffer::setData(void* data) {
-    this->_data = data;
-    glBufferData(this->_type, sizeof(this->_data), this->_data, this->usage);
+    this->data = data;
+    glBufferData(this->type, sizeof(data), data, this->usage);
 }
 
 OpenGLVertexArray::OpenGLVertexArray(float* vertices, int vertexCount) {
