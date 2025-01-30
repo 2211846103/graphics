4bb9a82d57b9fe6c95816137cc36d7bb7205c776
MIbrahim9276 <m.ibrahm9276@gmail.com>
2025-01-30 21:09:19 +0200

    Removed Current Texture Usage

--- include/graphics_api.hpp
+++ include/graphics_api.hpp
@@ -4,7 +4,6 @@
 #include <window.hpp>
 #include <shader.hpp>
 #include <graphics_components.hpp>
-#include <texture.hpp>
 
 namespace Graphics {
     // Abstract Factory
@@ -15,9 +14,6 @@ namespace Graphics {
             virtual Buffer* createBuffer(BufferType type, void* data, size_t size, BufferUsage usage) = 0;
             virtual Shader* createShader(const char* vPath, const char* fPath) = 0;
             virtual VertexArray* createVertexArray(float* vertices, size_t size) = 0;
-            virtual Texture2D* createTexture2D(const char* path) = 0;
-            virtual Texture3D* createTexture3D(const char* path[], size_t size) = 0;
-            virtual TextureCube* createTextureCube(const char* path[], size_t size) = 0;
     };
 
     // Concrete Factories
@@ -27,8 +23,5 @@ namespace Graphics {
             Buffer* createBuffer(BufferType type, void* data, size_t size, BufferUsage usage) override;
             Shader* createShader(const char* vPath, const char* fPath) override;
             VertexArray* createVertexArray(float* vertices, size_t size) override;
-            Texture2D* createTexture2D(const char* path) override;
-            Texture3D* createTexture3D(const char* path[], size_t size) override;
-            TextureCube* createTextureCube(const char* path[], size_t size) override;
     };
 }
