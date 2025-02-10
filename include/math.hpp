d23f027ba285e6803e5a02f64396d7c85eb3e182
IbrahimElsharif <Elsharif4458@outlook.com>
2025-02-11 00:22:18 +0200

    added vec4, mat4, mat3, mat2 classes

--- include/math.hpp
+++ include/math.hpp
@@ -4,13 +4,61 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace Math {
+    class Vec4 {
+        public:
+            glm::vec4 data;
+
+            // Constructors
+            Vec4();
+            Vec4(float x, float y, float z, float w);
+            Vec4(const glm::vec4& vec);
+
+            // Accessors
+            float x();
+            float y();
+            float z();
+            float w();
+
+            void setX(float x);
+            void setY(float y);
+            void setZ(float z);
+            void setW(float w);
+
+            // Operators
+            Vec4 operator+(const Vec4& other);
+            Vec4 operator-(const Vec4& other);
+            Vec4 operator*(float scalar);
+            Vec4 operator/(float scalar);
+            Vec4& operator+=(const Vec4& other);
+            Vec4& operator-=(const Vec4& other);
+            Vec4& operator*=(float scalar);
+            Vec4& operator/=(float scalar);
+            bool operator==(const Vec4& other);
+            bool operator!=(const Vec4& other);
+
+            // Conversions
+            operator glm::vec4();
+            float* toFloat();
+
+            // Magnitude and normalization
+            float length();
+            Vec4 normalize();
+
+            // Dot and cross product
+            float dot(const Vec4& other);
+            Vec4 cross(const Vec4& other);
+
+            // Stream operator
+            friend std::ostream& operator<<(std::ostream& os, Vec4& vec);
+    };
+
     class Vec3 {
-        private:
+        public:
             glm::vec3 data;
 
-        public:
             // Constructors
             Vec3();
             Vec3(float x, float y, float z);
@@ -47,16 +95,16 @@ namespace Math {
 
             // Dot and cross product
             float dot(const Vec3& other);
+            Vec3 cross(const Vec3& other);
             
             // Stream operator
             friend std::ostream& operator<<(std::ostream& os, Vec3& vec);
     };
 
     class Vec2 {
-        private:
+        public:
             glm::vec2 data;
 
-        public:
             // Constructors
             Vec2();
             Vec2(float x, float y);
@@ -90,7 +138,113 @@ namespace Math {
             operator glm::vec2();
             float* toFloat();
 
+            // Dot and cross product
+            float dot(const Vec2& other);
+            float cross(const Vec2& other);
+
             // Friend functions
             friend std::ostream& operator<<(std::ostream& os, Vec2& vec);
+    };
+    
+    class Mat4 {
+        public:
+            glm::mat4 data;
+
+            // Constructors
+            Mat4();
+            Mat4(const glm::mat4& mat);
+
+            // Accessors
+            float* toFloat();
+            float& operator()(int row, int col); // Allows for modification
+            const float& operator()(int row, int col) const; // for read only
+
+            // Transformations methods
+            static Mat4* identity();
+            static Mat4* translation(const Mat4& mat, const Vec3& v);
+            static Mat4* rotation(const Mat4& mat, float angle, const Vec3& axis);
+            static Mat4* scale(const Mat4& mat, const Vec3& v);
+
+            // Operators
+            Mat4 operator+(Mat4& other);
+            Mat4 operator-(Mat4& other);
+            Mat4 operator*(Mat4& other);
+            Vec4 operator*(Vec4& vec);
+            Mat4 operator*(float scalar);
+
+            // Methods
+            Mat4 transpose();
+            Mat4 inverse();
+
+            // Print
+            void print();
+
+    };
+
+    class Mat3 {
+        public:
+            glm::mat3 data;
+
+            // Constructors
+            Mat3();
+            Mat3(const glm::mat3& mat);
+
+            // Accessors
+            float* toFloat();
+            float& operator()(int row, int col); // Allows for modification
+            const float& operator()(int row, int col) const; // for read only
+
+            // Transformations methods
+            static Mat3* identity();
+            static Mat3* translation(const Mat3& mat, Vec2& v);
+            static Mat3* rotation(const Mat3& mat, float angle);
+            static Mat3* scale(const Mat3& mat, Vec2& v);
+
+            // Operators
+            Mat3 operator+(Mat3& other);
+            Mat3 operator-(Mat3& other);
+            Mat3 operator*(Mat3& other);
+            Vec3 operator*(Vec3& vec);
+            Mat3 operator*(float scalar);
+
+            //methods
+            Mat3 transpose();
+            Mat3 inverse();
+
+            // Print
+            void print();
+    };
+
+    class Mat2 {
+        public:
+            glm::mat2 data;
+
+            // Constructors
+            Mat2();
+            Mat2(const glm::mat2& mat);
+
+            // Accessors
+            float* toFloat();
+            float& operator()(int row, int col); // Allows for modification
+            const float& operator()(int row, int col) const; // for read only
+
+            // Transformations methods
+            static Mat2* identity();
+            static Mat2* rotation(const Mat2& mat, float angle);
+            static Mat2* scale(const Mat2& mat, Vec2& v);
+
+            // Operators
+            Mat2 operator+(Mat2& other);
+            Mat2 operator-(Mat2& other);
+            Mat2 operator*(Mat2& other);
+            Vec2 operator*(Vec2& vec);
+            Mat2 operator*(float scalar);
+
+            //methods
+            Mat2 transpose();
+            Mat2 inverse();
+
+            // Print
+            void print();
     };
 }   
--- src/math.cpp
+++ src/math.cpp
@@ -2,7 +2,56 @@
 
 using namespace Math;
 
-//ructors
+// Constructors
+Vec4::Vec4() : data(0.0f) {}
+Vec4::Vec4(float x, float y, float z, float w) : data(x, y, z, w) {}
+Vec4::Vec4(const glm::vec4& vec) : data(vec) {}
+
+// Accessors
+float Vec4::x() { return data.x; }
+float Vec4::y() { return data.y; }
+float Vec4::z() { return data.z; }
+float Vec4::w() { return data.w; }
+
+void Vec4::setX(float x) { data.x = x; }
+void Vec4::setY(float y) { data.y = y; }
+void Vec4::setZ(float z) { data.z = z; }
+void Vec4::setW(float w) { data.w = w; }
+
+// Operators
+Vec4 Vec4::operator+(const Vec4& other) { return Vec4(data + other.data); }
+Vec4 Vec4::operator-(const Vec4& other) { return Vec4(data - other.data); }
+Vec4 Vec4::operator*(float scalar) { return Vec4(data * scalar); }
+Vec4 Vec4::operator/(float scalar) { return Vec4(data / scalar); }
+Vec4& Vec4::operator+=(const Vec4& other) { data += other.data; return *this; }
+Vec4& Vec4::operator-=(const Vec4& other) { data -= other.data; return *this; }
+Vec4& Vec4::operator*=(float scalar) { data *= scalar; return *this; }
+Vec4& Vec4::operator/=(float scalar) { data /= scalar; return *this; }
+bool Vec4::operator==(const Vec4& other) { return data == other.data; }
+bool Vec4::operator!=(const Vec4& other) { return data != other.data; }
+
+// Conversions
+Vec4::operator glm::vec4() { return data; }
+float* Vec4::toFloat() {return glm::value_ptr(data);}
+
+// Magnitude and normalization
+float Vec4::length() { return glm::length(data); }
+Vec4 Vec4::normalize() { return Vec4(glm::normalize(data)); }
+
+// Dot product
+float Vec4::dot(const Vec4& other) { return glm::dot(data, other.data); }
+Vec4 Vec4::cross(const Vec4& other) { 
+    glm::vec3 result = glm::cross(glm::vec3(data), glm::vec3(other.data));
+    return Vec4(result.x, result.y, result.z, 0.0f);
+}
+
+// Stream operator
+std::ostream& operator<<(std::ostream& os, Vec4& vec) {
+    os << "(" << vec.x() << ", " << vec.y() << ", " << vec.z() << ", " << vec.w() << ")";
+    return os;
+}
+
+// Constructors
 Vec3::Vec3() : data(0.0f) {}
 Vec3::Vec3(float x, float y, float z) : data(x, y, z) {}
 Vec3::Vec3(const glm::vec3& vec) : data(vec) {}
@@ -38,6 +87,7 @@ Vec3 Vec3::normalize() { return Vec3(glm::normalize(data)); }
 
 // Dot product
 float Vec3::dot(const Vec3& other) { return glm::dot(data, other.data); }
+Vec3 Vec3::cross(const Vec3& other) { return glm::cross(data, other.data); }
 
 // Stream operator
 std::ostream& operator<<(std::ostream& os, Vec3& vec) {
@@ -78,8 +128,153 @@ Vec2 Vec2::normalize() { return Vec2(glm::normalize(data)); }
 Vec2::operator glm::vec2() { return data; }
 float* Vec2::toFloat() {return glm::value_ptr(data);}
 
+float Vec2::dot(const Vec2& other) { return glm::dot(data, other.data); }
+float Vec2::cross(const Vec2& other) { 
+    return data.x * other.data.y - data.y * other.data.x;
+}
+
 // Friend functions
 std::ostream& operator<<(std::ostream& os, Vec2& vec) {
     os << "Vec2(" << vec.x() << ", " << vec.y() << ")";
     return os;
+}
+
+// Constructors
+Mat4::Mat4() : data(1.0f) {}
+Mat4::Mat4(const glm::mat4& mat) : data(mat) {}
+
+// Accessors
+float* Mat4::toFloat() { return glm::value_ptr(data); }
+float& Mat4::operator()(int i, int j) { return data[i][j]; }
+const float& Mat4::operator()(int i, int j) const { return data[i][j]; }
+
+// Trastransformations methods
+Mat4* Mat4::identity() { return new Mat4(glm::mat4(1.0f)); }
+Mat4* Mat4::translation(const Mat4& mat, const Vec3& v) { return new Mat4(glm::translate(mat.data, v.data)); }
+Mat4* Mat4::rotation(const Mat4& mat, float angle, const Vec3& axis) { return new Mat4(glm::rotate(mat.data, angle, axis.data)); }
+Mat4* Mat4::scale(const Mat4& mat, const Vec3& v) { return new Mat4(glm::scale(mat.data, v.data)); }
+
+// operators
+Mat4 Mat4::operator+(Mat4& other) { return Mat4(data + other.data); }
+Mat4 Mat4::operator-(Mat4& other) { return Mat4(data - other.data); }
+Mat4 Mat4::operator*(Mat4& other) { return Mat4(data * other.data); }
+Vec4 Mat4::operator*(Vec4& vec) { return Vec4(data * vec); }
+Mat4 Mat4::operator*(float scalar) { return Mat4(data * scalar); }
+
+// methods
+Mat4 Mat4::transpose() { return Mat4(glm::transpose(data)); }
+Mat4 Mat4::inverse() { return Mat4(glm::inverse(data)); }
+
+// Print
+void Mat4::print() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << data[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Constructors
+Mat3::Mat3() : data(1.0f) {}
+Mat3::Mat3(const glm::mat3& mat) : data(mat) {}
+
+// Accessors
+float* Mat3::toFloat() { return glm::value_ptr(data); }
+float& Mat3::operator()(int i, int j) { return data[i][j]; }
+const float& Mat3::operator()(int i, int j) const { return data[i][j]; }
+
+// Trastransformations methods
+Mat3* Mat3::identity() { return new Mat3(glm::mat3(1.0f)); }
+Mat3* Mat3::translation(const Mat3& mat, Vec2& v) { 
+    glm::mat3 translationMat(
+            1.0f, 0.0f, v.x(),
+            0.0f, 1.0f, v.y(),
+            0.0f, 0.0f, 1.0f
+        );
+    return new Mat3(mat.data * translationMat);
+ }
+Mat3* Mat3::rotation(const Mat3& mat, float angle) { 
+    glm::mat3 rotationMat(
+            cos(angle), -sin(angle), 0.0f,
+            sin(angle), cos(angle), 0.0f,
+            0.0f, 0.0f, 1.0f
+        );
+    return new Mat3(mat.data * rotationMat);
+ }
+Mat3* Mat3::scale(const Mat3& mat, Vec2& v) { 
+    glm::mat3 scaleMat(
+            v.x(), 0.0f, 0.0f,
+            0.0f, v.y(), 0.0f,
+            0.0f, 0.0f, 1.0f
+        );
+    return new Mat3(mat.data * scaleMat);
+}
+
+// Operators
+Mat3 Mat3::operator+(Mat3& other) { return Mat3(data + other.data); }
+Mat3 Mat3::operator-(Mat3& other) { return Mat3(data - other.data); }
+Mat3 Mat3::operator*(Mat3& other) { return Mat3(data * other.data); }
+Vec3 Mat3::operator*(Vec3& vec) { return Vec3(data * vec); }
+Mat3 Mat3::operator*(float scalar) { return Mat3(data * scalar); }
+
+// Methods
+Mat3 Mat3::transpose() { return Mat3(glm::transpose(data)); }
+Mat3 Mat3::inverse() { return Mat3(glm::inverse(data)); }
+
+// Print
+void Mat3::print() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << data[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Constructors
+Mat2::Mat2() : data(1.0f) {}
+Mat2::Mat2(const glm::mat2& mat) : data(mat) {}
+
+// Accessors
+float* Mat2::toFloat() { return glm::value_ptr(data); }
+float& Mat2::operator()(int i, int j) { return data[i][j]; }
+const float& Mat2::operator()(int i, int j) const { return data[i][j]; }
+
+// Trastransformations methods
+Mat2* Mat2::identity() { return new Mat2(glm::mat2(1.0f)); }
+Mat2* Mat2::rotation(const Mat2& mat, float angle) { 
+    glm::mat2 rotationMat(
+            cos(angle), -sin(angle),
+            sin(angle), cos(angle)
+        );
+    return new Mat2(mat.data * rotationMat);
+ }
+ Mat2* Mat2::scale(const Mat2& mat, Vec2& v) { 
+    glm::mat2 scaleMat(
+            v.x(), 0.0f,
+            0.0f, v.y()
+        );
+    return new Mat2(mat.data * scaleMat);
+ }
+
+// Operators
+Mat2 Mat2::operator+(Mat2& other) { return Mat2(data + other.data); }
+Mat2 Mat2::operator-(Mat2& other) { return Mat2(data - other.data); }
+Mat2 Mat2::operator*(Mat2& other) { return Mat2(data * other.data); }
+Vec2 Mat2::operator*(Vec2& vec) { return Vec2(data * vec); }
+Mat2 Mat2::operator*(float scalar) { return Mat2(data * scalar); }
+
+// Methods
+Mat2 Mat2::transpose() { return Mat2(glm::transpose(data)); }
+Mat2 Mat2::inverse() { return Mat2(glm::inverse(data)); }
+
+// Print
+void Mat2::print() {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            std::cout << data[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
