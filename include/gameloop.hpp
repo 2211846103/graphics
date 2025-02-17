2c949f50bfb6e2f71e2018bdd7c32679229e00b9
MIbrahim9276 <m.ibrahm9276@gmail.com>
2025-02-17 21:08:41 +0200

    Added File to Include all of the Game Loop

---
+++ include/gameloop.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <window.hpp>
+#include <mesh.hpp>
+#include <Input.hpp>
+
+namespace Engine {
+    void Start();
+
+    void Update(float dt);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 // Includes
-#include <window.hpp>
-#include <mesh.hpp>
-#include <Input.hpp>
-#include <ctime>
+#include <gameloop.hpp>
 
 using namespace Graphics;
 using namespace Engine;
@@ -17,83 +14,22 @@ int main(int argc, char* argv[]) {
     graphics.enableZBuffer();
     graphics.setClearColor({0.2, 0.2, 0.2});
     SceneManager::setGraphicsAPI(&graphics);
-    Scene mainScene;
-    SceneManager::setCurrentScene(&mainScene);
-
-    // Code
-    Vec3 cubePositions[] = {
-        { 0.0f,  0.0f,  0.0f },
-        { 2.0f,  5.0f, -15.0f },
-        { -1.5f, -2.2f, -2.5f },
-        { -3.8f, -2.0f, -12.3f },
-        { 2.4f, -0.4f, -3.5f },
-        { -1.7f,  3.0f, -7.5f },
-        { 1.3f, -2.0f, -2.5f },
-        { 1.5f,  2.0f, -2.5f },
-        { 1.5f,  0.2f, -1.5f },
-        { -1.3f,  1.0f, -1.5f }
-    };    
-    for (int i = 0; i < 10; i++) {
-        // Cubes to render
-        GameObject* cube = mainScene.createPlane();
-
-        // This Line is not Necessary anymore
-        cube->getComponent<Mesh>()->material->setAlbedo("../res/images/test.jpg");
-
-        Transform* transform = cube->getComponent<Transform>();
-        transform->position = cubePositions[i];
-        transform->rotation = Vec3(10 * i, 15 * i, 20 * i);
-    }
-
-    // Camera
-    GameObject* cameraObj = mainScene.createCamera();
-    mainScene.setCameraActive(cameraObj);
-
-    Camera* camera = cameraObj->getComponent<Camera>();
-    Transform* cameraTrans = cameraObj->getComponent<Transform>();
-    cameraTrans->position = Vec3(0, 0, -15);
 
     // Calculating DeltaTime
-    float dt = 0;
-    const float cameraSpeed = 10;
-    float mouseSensitivity = 0.1f;
-    float lastX = 400, lastY = 300;
-    float xoffset, yoffset;
-
+    float dt = 0;  
     
-    mainScene.initGameObjects();
+    Start();
+    
+    Scene* current = SceneManager::getCurrentScene();
+    if (current) current->initGameObjects();
+
     while (!window.shouldClose()) {
         graphics.clear();
-        
-        if (Input::isKeyPressed(W)) cameraTrans->position -= dt * cameraSpeed * camera->front.data;
-        if (Input::isKeyPressed(S)) cameraTrans->position += dt * cameraSpeed * camera->front.data;
-        if (Input::isKeyPressed(A)) cameraTrans->position += dt * cameraSpeed * (camera->front.cross(camera->up)).normalize().data;
-        if (Input::isKeyPressed(D)) cameraTrans->position -= dt * cameraSpeed * (camera->front.cross(camera->up)).normalize().data;
-        if (Input::isKeyPressed(LSHIFT)) cameraTrans->position -= dt * cameraSpeed * camera->up.data;
-        if (Input::isKeyPressed(SPACE)) cameraTrans->position += dt * cameraSpeed * camera->up.data;
-        
-        Vec2 mousePos = Input::getMousePosition();
-        Vec3 direction;
-        
-        xoffset = mousePos.data.x - lastX;
-        yoffset = mousePos.data.y - lastY;
-        lastX = mousePos.data.x;
-        lastY = mousePos.data.y;
 
-        xoffset *= mouseSensitivity;
-        yoffset *= mouseSensitivity;
+        Update(dt);
 
-        cameraTrans->rotation.data.x += xoffset;
-        cameraTrans->rotation.data.y += yoffset;
-        
-        direction.data.x = cos(glm::radians(cameraTrans->rotation.data.x)) * cos(glm::radians(cameraTrans->rotation.data.y));
-        direction.data.y = sin(glm::radians(cameraTrans->rotation.data.y));
-        direction.data.z = sin(glm::radians(cameraTrans->rotation.data.x)) * cos(glm::radians(cameraTrans->rotation.data.y));
-        camera->front = direction.normalize();
-
-
-        mainScene.updateGameObjects(dt);
-        mainScene.renderGameObjects();
+        if (current) current->updateGameObjects(dt);
+        if (current) current->renderGameObjects();
 
         dt = window.update();
     }
