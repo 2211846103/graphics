88bf9f5c7fe365724e42e2d237cd5df757ea0eb6
MO Elkilani <mohammedalkilanee@gmail.com>
2025-04-17 18:18:21 +0200

    metal window finally works fuck u zinjibar :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,28 @@
 #include <renderer/window.hpp>
 
+<<<<<<< Updated upstream
 
 using namespace engine;
+=======
+using namespace engine::rendering;
+>>>>>>> Stashed changes
 
 int main() {
-#ifdef ENGINE_COMPILE_DIRECTX
-  VulkanWindow window(800, 600, "OpenGL Window");
-  window.initWindow();
+#ifdef ENGINE_COMPILE_METAL
+    std::cout << "Initializing Metal window..." << std::endl;
 
-  float dt = 0;
+    MetalWindow window(800, 600, "Metal Window");
+    window.initWindow();
 
-  while (!window.shouldClose()) {
-    dt = window.update();
-  }
+    float dt = 0;
 
-  glfwTerminate();
-  std::cout << "Window closed successfully." << std::endl;
+    while (!window.shouldClose()) {
+        std::cout << "Running update loop..." << std::endl;
+        dt = window.update();
+    }
+
+    std::cout << "Window closed successfully." << std::endl;
 #endif
 
-  return 0;
+    return 0;
 }
