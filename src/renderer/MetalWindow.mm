a5f95f9824d9060951d7a17807e35a484f3ee5b9
MO Elkilani <mohammedalkilanee@gmail.com>
2025-04-19 20:51:24 +0200

    fixed with the shader tingy :)

--- src/renderer/MetalWindow.mm
+++ src/renderer/MetalWindow.mm
@@ -3,13 +3,16 @@
 #import <Metal/Metal.h>
 
 #include <renderer/window.hpp>
+#include <iostream>
+
 
 using namespace engine::rendering;
 
 #ifdef ENGINE_COMPILE_METAL
 
 MetalWindow::MetalWindow(int width, int height, const char* name)
-: _width(width), _height(height), _windowName(name), _window(nil), _contentView(nil), _metalLayer(nil) {}
+: _width(width), _height(height), _windowName(name), _window(nil), _contentView(nil),
+  _metalLayer(nil), _device(nil), _commandQueue(nil), _shaderLibrary(nil), _pipelineState(nil), shaderLoaded(false) {}
 
 MetalWindow::~MetalWindow() {
     if (_window) {
@@ -20,12 +23,9 @@ MetalWindow::~MetalWindow() {
 
 void MetalWindow::initWindow() {
     @autoreleasepool {
-
         NSApplication* app = [NSApplication sharedApplication];
 
-
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-
 
         NSRect frame = NSMakeRect(0, 0, _width, _height);
         _window = [[NSWindow alloc] initWithContentRect:frame
@@ -38,20 +38,68 @@ void MetalWindow::initWindow() {
         [_window makeKeyAndOrderFront:nil];
 
         _contentView = [_window contentView];
+
+        _device = MTLCreateSystemDefaultDevice();
+        _commandQueue = [_device newCommandQueue];
+
         _metalLayer = [CAMetalLayer layer];
-        _metalLayer.device = MTLCreateSystemDefaultDevice();
+        _metalLayer.device = _device;
         _metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
         _metalLayer.framebufferOnly = YES;
         _metalLayer.drawableSize = CGSizeMake(_width, _height);
         _metalLayer.contentsScale = [[NSScreen mainScreen] backingScaleFactor];
 
-
         _contentView.wantsLayer = YES;
         _contentView.layer = _metalLayer;
 
-
         [NSApp finishLaunching];
         [NSApp activateIgnoringOtherApps:YES];
+    }
+}
+
+void MetalWindow::loadShader(const std::string& source) {
+    if (shaderLoaded) return;
+
+    std::cout << "Loading MetalShader" << std::endl;
+
+    @autoreleasepool {
+        NSError* error = nil;
+
+        NSString* shaderSrc = [NSString stringWithUTF8String:source.c_str()];
+        _shaderLibrary = [_device newLibraryWithSource:shaderSrc options:nil error:&error];
+
+        if (!_shaderLibrary) {
+            std::cout << "ERROR::METAL::SHADER::COMPILATION_FAILED\n" << error.localizedDescription.UTF8String << std::endl;
+            return;
+        }
+
+        id<MTLFunction> vertexFunction = [_shaderLibrary newFunctionWithName:@"vertex_main"];
+        id<MTLFunction> fragmentFunction = [_shaderLibrary newFunctionWithName:@"fragment_main"];
+
+        if (!vertexFunction || !fragmentFunction) {
+            std::cout << "ERROR::METAL::SHADER::FUNCTION_NOT_FOUND" << std::endl;
+            return;
+        }
+
+        MTLRenderPipelineDescriptor* pipelineDesc = [[MTLRenderPipelineDescriptor alloc] init];
+        pipelineDesc.vertexFunction = vertexFunction;
+        pipelineDesc.fragmentFunction = fragmentFunction;
+        pipelineDesc.colorAttachments[0].pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+        _pipelineState = [_device newRenderPipelineStateWithDescriptor:pipelineDesc error:&error];
+
+        if (!_pipelineState) {
+            std::cout << "ERROR::METAL::PIPELINE_STATE::FAILED\n" << error.localizedDescription.UTF8String << std::endl;
+            return;
+        }
+
+        shaderLoaded = true;
+    }
+}
+
+void MetalWindow::useShader(id<MTLRenderCommandEncoder> encoder) {
+    if (_pipelineState && encoder) {
+        [encoder setRenderPipelineState:_pipelineState];
     }
 }
 
@@ -61,7 +109,6 @@ bool MetalWindow::shouldClose() {
 
 float MetalWindow::update() {
     @autoreleasepool {
-
         NSEvent* event;
         while ((event = [NSApp nextEventMatchingMask:NSEventMaskAny
                                              untilDate:[NSDate dateWithTimeIntervalSinceNow:0.01]
