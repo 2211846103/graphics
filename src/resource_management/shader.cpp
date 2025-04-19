99d2cb7f1c2bb399eee98e19cb6da539f7245ec3
MO Elkilani <mohammedalkilanee@gmail.com>
2025-04-19 21:38:23 +0200

    added the metal on shaders.cpp tingy :)

--- src/resource_management/shader.cpp
+++ src/resource_management/shader.cpp
@@ -96,3 +96,86 @@ void OpenGLShader::useShader() {
   glUseProgram(shader_program_id);
 }
 #endif
+
+#ifdef ENGINE_COMPILE_METAL
+#import <Metal/Metal.h>
+#import <Foundation/Foundation.h>
+
+using namespace engine::resource_management;
+
+MetalShader::MetalShader(std::shared_ptr<File> vertex, std::shared_ptr<File> fragment, std::string name, bool lazy) :
+Shader(vertex, fragment, name, lazy) {
+  if (!lazy) this->load();
+}
+
+MetalShader::~MetalShader() {
+  this->unload();
+}
+
+void MetalShader::load() {
+  if (this->loaded) return;
+
+  std::cout << "Loading MetalShader" << std::endl;
+
+  id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+  if (!device) {
+    std::cout << "ERROR::METAL::NO_DEVICE" << std::endl;
+    return;
+  }
+
+  std::string vertex_src = vertex_shader->read();
+  std::string fragment_src = fragment_shader->read();
+  std::string full_src = vertex_src + "\n" + fragment_src;
+
+  NSString* msl = [NSString stringWithUTF8String:full_src.c_str()];
+  NSError* error = nil;
+  library = [device newLibraryWithSource:msl options:nil error:&error];
+
+  if (!library) {
+    std::cout << "ERROR::METAL::LIBRARY_FAILED\n" << error.localizedDescription.UTF8String << std::endl;
+    return;
+  }
+
+  vertex_function = [library newFunctionWithName:@"vertex_main"];
+  fragment_function = [library newFunctionWithName:@"fragment_main"];
+
+  if (!vertex_function || !fragment_function) {
+    std::cout << "ERROR::METAL::FUNCTION_NOT_FOUND" << std::endl;
+    return;
+  }
+
+  MTLRenderPipelineDescriptor* descriptor = [[MTLRenderPipelineDescriptor alloc] init];
+  descriptor.vertexFunction = vertex_function;
+  descriptor.fragmentFunction = fragment_function;
+  descriptor.colorAttachments[0].pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+  pipeline_state = [device newRenderPipelineStateWithDescriptor:descriptor error:&error];
+  if (!pipeline_state) {
+    std::cout << "ERROR::METAL::PIPELINE_STATE_FAILED\n" << error.localizedDescription.UTF8String << std::endl;
+    return;
+  }
+
+  this->loaded = true;
+}
+
+void MetalShader::unload() {
+  if (!this->loaded) return;
+
+  library = nil;
+  vertex_function = nil;
+  fragment_function = nil;
+  pipeline_state = nil;
+
+  std::cout << "Unloading MetalShader" << std::endl;
+
+  this->loaded = false;
+}
+
+void MetalShader::useShader() {
+  this->load();
+  extern id<MTLRenderCommandEncoder> currentEncoder;
+  if (pipeline_state && currentEncoder) {
+    [currentEncoder setRenderPipelineState:pipeline_state];
+  }
+}
+#endif
